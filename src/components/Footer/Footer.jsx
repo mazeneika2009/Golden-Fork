0536bd80cd4ffbaee0d67ce76a960e3fa71d96f5
mazeneika2009 <mazeneika2009@gmail.com>
2026-02-22 23:34:20 +0200

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 
   return (
     <footer className="footer" id="contact">
-      < div className="footer-container">
+      <div className="footer-container">
         <div className="footer-col">
           <h4>{t('footer.brand')}</h4>
           <p>{t('footer.tagline')}</p>
@@ -16,19 +16,13 @@ const Footer = () => {
         <div className="footer-col">
           <h4>{t('footer.contactTitle')}</h4>
           <p>{t('footer.address')}</p>
-          <p>+1 (212) 555-0199</p>
+          <p>{t('footer.phone')}</p>
           <p>{t('footer.email')}</p>
         </div>
         <div className="footer-col">
           <h4>{t('footer.hoursTitle')}</h4>
           <p>{t('footer.hours')}</p>
           <p>{t('footer.happyHour')}</p>
-        </div>
-        <div className="footer-col">
-          <h4>{t('footer.staff')}</h4>
-          <p><Link to="/admin" style={{ color: 'inherit', textDecoration: 'none' }}>{t('footer.admin')}</Link></p>
-          <p><Link to="/kitchen" style={{ color: 'inherit', textDecoration: 'none' }}>{t('footer.kitchen')}</Link></p>
-          <p><Link to="/cashier" style={{ color: 'inherit', textDecoration: 'none' }}>{t('footer.cashier')}</Link></p>
         </div>
       </div>
       <div className="footer-bottom">
